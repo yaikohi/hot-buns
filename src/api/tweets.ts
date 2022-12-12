8d8c4e1075892c994845dc09c27642fd196e8beb
yaikohi <erik.beem@hotmail.com>
2022-12-12 13:11:53 +0100

    Fetch works

--- src/api/tweets.ts
+++ src/api/tweets.ts
@@ -3,9 +3,27 @@
 // 2. Fetch the images
 
 import { Hono } from "hono";
+import { env } from "process";
 
 export const tweets = new Hono();
 
-tweets.get("/", (c) => {
-  return c.text("tweets-root!");
+tweets.get("/", async (c) => {
+  const token = Bun.env.TWITTER_API_BEARER_TOKEN as string; // works
+  const username = "hhhghq0";
+  const url = `https://api.twitter.com/2/users/by/username/${username}`;
+  const res = await fetch(url, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const data = ((await res.json()) as any).data;
+
+  console.log(data);
+
+  return c.json({ data });
 });
+
+// export const getMyOwnTweets = () => {}
+
+// const token = `${Bun.env}`
+//--header 'Authorization: Bearer XXXXXX'
