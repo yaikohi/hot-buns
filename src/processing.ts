958abe215b70d4f89430cad54b79b34a20f3f3f5
yaikohi <erik.beem@hotmail.com>
2022-12-15 14:48:55 +0100

    Fix processor?

--- src/processing.ts
+++ src/processing.ts
@@ -2,46 +2,49 @@ import { TwitterResponseData } from "./types";
 
 /**
  * Takes a 'full' twitter api response.
- * 
+ *
  * 1. Adds the media objects to the tweet containing them;
  * 2. and then adds the tweets to the user that tweeted them;
  */
 export const processTweets = (twitterResponse: TwitterResponseData) => {
-  const users = twitterResponse.includes.users.map((user) => user);
-  const tweets = twitterResponse.data.map((tweet) => tweet);
-  const media = twitterResponse.includes.media.map((media) => media);
+  const usersList = twitterResponse.includes.users.map((user) => user);
+  const tweetsList = twitterResponse.data.map((tweet) => tweet);
+  const mediaList = twitterResponse.includes.media.map((media) => media);
 
-  const tweetsWithMedia = tweets.map((tweet) => {
+  const tweetsWithMedia = tweetsList.map((tweet) => {
+    // return the tweet if there aren't any media_keys
     if (!tweet?.attachments?.media_keys) {
       return tweet;
     }
 
+    // if there are multiple media keys, handle accordingly
     if (tweet.attachments.media_keys.length > 1) {
       return {
         ...tweet,
         media: tweet.attachments.media_keys.filter((mkey) => {
-          media.map((mediaItem) => mediaItem.media_key === mkey);
+          return mediaList.filter((mediaItem) => mediaItem.media_key === mkey);
         }),
       };
     }
 
+    // if there is only one media_key, handle accordingly
     if (tweet.attachments.media_keys.length === 1) {
       return {
         ...tweet,
-        media: media.filter(
-          (medium) => medium.media_key === tweet.attachments.media_keys[0]
+        media: mediaList.filter(
+          (mediaItem) => mediaItem.media_key === tweet.attachments.media_keys[0]
         ),
       };
     }
     return {
       ...tweet,
-      media: media.map(
-        (thing) => thing.media_key === tweet?.attachments?.media_keys[0]
+      media: mediaList.map(
+        (mediaItem) => mediaItem.media_key === tweet?.attachments?.media_keys[0]
       ),
     };
   });
 
-  return users.map((user) => ({
+  return usersList.map((user) => ({
     ...user,
     tweets: tweetsWithMedia.filter((tweet) => tweet.author_id === user.id),
   }));
